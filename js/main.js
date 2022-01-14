5ab4ce2d68fbcaf475318472cd738ad4823b1334
ashmeet-singh <76838543+ashmeet-singh@users.noreply.github.com>
2022-01-15 05:09:58 +0530

    Small change

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ function upload() {
     logView = document.getElementById('LogView')
 
     var filePath = document.getElementById('DirInput').value + fileInput.files[0].name;
+
+    logViewAddLine('Preparing to upload file: ' + filePath);
+
     var xhttp = new XMLHttpRequest();
 
     xhttp.addEventListener("load", transferComplete);
@@ -17,8 +20,8 @@ function upload() {
         logViewAddLine('File upload failed: ' + filePath);
     }
 
-    logViewAddLine('Preparing to upload file: ' + filePath);
     xhttp.open('POST', encodeURI('/upload/storage/' + filePath), true)
     xhttp.send(fileInput.files[0])
+
     logViewAddLine('Uploading file: ' + filePath);
 }
